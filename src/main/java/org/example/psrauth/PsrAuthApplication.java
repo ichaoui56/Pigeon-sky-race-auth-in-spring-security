105c5eccfd1fb4358e3e560252f023534a10deab
Ilyas chaoui <144786151+ichaoui56@users.noreply.github.com>
2024-12-04 09:53:14 +0100

    Link local project to GitHub repository and add initial project files

---
+++ src/main/java/org/example/psrauth/PsrAuthApplication.java
@@ -0,0 +1,13 @@
+package org.example.psrauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsrAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PsrAuthApplication.class, args);
+    }
+
+}
