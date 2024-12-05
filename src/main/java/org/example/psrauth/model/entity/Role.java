9c1af57a720f29bdc630110006d2b7dab6beabb6
Ilyas chaoui <144786151+ichaoui56@users.noreply.github.com>
2024-12-05 11:18:59 +0100

    PRS-9 add all entities nedded

---
+++ src/main/java/org/example/psrauth/model/entity/Role.java
@@ -0,0 +1,29 @@
+package org.example.psrauth.model.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.example.psrauth.model.RoleType;
+
+import java.util.*;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    @OneToMany(mappedBy = "role", fetch = FetchType.LAZY)
+    @JsonIgnore
+    private List<User> users;
+}
